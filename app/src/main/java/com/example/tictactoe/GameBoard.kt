d95ee3570ab32b3888b2cd3ccd59e029bd04e87b
Nish1779 <64070976+Nish1779@users.noreply.github.com>
2022-03-12 18:48:20 -0500

    week 6
    week 6 Done

--- app/src/main/java/com/example/tictactoe/GameBoard.kt
+++ app/src/main/java/com/example/tictactoe/GameBoard.kt
@@ -2,12 +2,47 @@ package com.example.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class GameBoard : AppCompatActivity() {
+
+
+    var gamemodel = Nish_GameModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_board)
 
         setTitle("New Game ") // change karyu title page nu .....
     }
+
+
+
+    fun squareTouched(square: View)
+    {
+
+        if ((square as Button).text.length > 0){
+            // check kare 6 k X or O 6 k nai em iff hoy to go back )
+            return
+        }
+        val id = square.id
+        var squareIndex = 0
+        when (id){
+            R.id.button1 -> {squareIndex = 1}
+            R.id.button2 -> {squareIndex = 2}
+            R.id.button3 -> {squareIndex = 3}
+            R.id.button4 -> {squareIndex = 4}
+            R.id.button5 -> {squareIndex = 5}
+            R.id.button6 -> {squareIndex = 6}
+            R.id.button7 -> {squareIndex = 7}
+            R.id.button8 -> {squareIndex = 8}
+            R.id.button9 -> {squareIndex = 9}
+        }
+
+        val movePlayed = gamemodel.processTouch(squareIndex)
+        (square as Button).text = movePlayed
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/tictactoe/Nish_GameModel.kt
@@ -0,0 +1,61 @@
+package com.example.tictactoe
+
+class Nish_GameModel
+{
+    var movesPlayed = Array(10) {i -> "" }
+    var whoseTurn = Constants.X
+    var whoWon = ""
+    var numOfMovesPlayed = 0
+
+    // this is the 2D array .... this is how you create 2D array
+    val winningcombinations : Array<IntArray> = arrayOf(
+        intArrayOf(1, 2, 3),
+        intArrayOf(4, 5, 6),
+        intArrayOf(7, 8, 8),
+        intArrayOf(1,4,7),
+        intArrayOf(2,5,8),
+        intArrayOf(3,6,9),
+        intArrayOf(1,5,9),
+        intArrayOf(3,5,7)
+        )
+
+    // initialize winning Combination 2D array
+
+    fun processTouch(num: Int) : String {
+        numOfMovesPlayed++
+        val playingNow = whoseTurn
+        movesPlayed[num] = whoseTurn
+
+        if (whoseTurn == Constants.X)
+        {
+            whoseTurn = Constants.O
+        }
+        else
+        {
+            whoseTurn = Constants.X
+        }
+        return playingNow
+    }
+
+    fun isGameFinished() : Boolean{
+        if (numOfMovesPlayed < 5)
+        {
+            return false
+        }
+        // otherwise itreate over winningCombinations
+
+        
+
+    }
+
+}
+
+    class Constants
+    {
+        companion object
+        {
+            val X = "X"
+            val O = "O"
+        }
+    }
+
